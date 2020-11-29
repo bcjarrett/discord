dcbb4d21424d6050eb3954fd7a2479377db41121
Ben Jarrett <bcjarrett@gmail.com>
2020-11-29 13:43:06 -0500

    finalized initial functions

--- dheads.py
+++ dheads.py
@@ -23,6 +23,18 @@ headers = {
                   'Chrome/61.0.3163.100 Safari/537.36'}
 
 
+def _get_game(in_str, ctx):
+    in_str = in_str.lower()
+    try:
+        return 1, Game.get(Game.name == in_str, Game.finished == False)
+    except Game.DoesNotExist:
+        game = list(Game.select().where(Game.name.contains(in_str), Game.finished == False))
+        if len(game) > 1:
+            return 0, f'More than one game returned for "{in_str}": {[i.name for i in game]}'
+        if len(game) == 1:
+            return 1, game[0]
+
+
 async def search_game(title, number_results=10, language_code='en'):
     google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(title.replace(" ", "+") + 'steam game',
                                                                           number_results + 1,
@@ -65,16 +77,17 @@ async def game_name_from_steam_id(steam_id):
 
 
 @bot.event
-async def on_ready():
-    for guild in bot.guilds:
-        if guild.id != GUILD:
-            bot._connection._remove_guild(guild)
-    guild = bot.get_guild(GUILD)
-
-    print(
-        f'{bot.user} is only connected to:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
+async def on_message(message):
+    if message.author != bot.user:
+        if random.randint(1, 70) == 69:
+            _ = ''
+            a = TextCount.get(text='p00p')
+            for i in range(a.counter):
+                _ += 'p00p '
+            await message.channel.send(_)
+            a.counter += 1
+            a.save()
+    await bot.process_commands(message)
 
 
 @bot.command(description='Clear p00p counter')
@@ -86,10 +99,27 @@ async def clear(ctx):
     return await ctx.send('Back to one p00p :cry:')
 
 
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        if guild.id == GUILD:
+            break
+            # bot._connection._remove_guild(guild)
+    guild = bot.get_guild(GUILD)
+
+    print(
+        f'{bot.user} is only connected to:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+    await bot.change_presence(activity=discord.Game(name="Oblivion"))
+
+
 @bot.command(description='Add a game to the list')
 async def add(ctx, *args):
     """
-    Adds a game to the list. Searches steam if no URL is provided.
+    Adds a game to the list
+    URL as optional parameter, searches steam if no URL is provided
+
         add Castle Crashers
         add Castle Crashers https://store.steampowered.com/app/204360/Castle_Crashers/
     """
@@ -125,7 +155,7 @@ async def add(ctx, *args):
         if steam_id:
             steam_id = steam_id[0].replace('/', '')
     try:
-        Game.get(Game.name == name.lower(), Game.played == False)
+        Game.get(Game.name == name.lower(), Game.started == False)
         return await ctx.send(f'Looks like "{name}" is already on the list')
     except Game.DoesNotExist:
         g = Game.create(name=name.lower(), added_by=ctx.author.id, url=url, steam_id=steam_id)
@@ -134,76 +164,105 @@ async def add(ctx, *args):
 
 
 @bot.command()
-async def remove(ctx, arg):
-    """Removes a game from the list"""""
-    try:
-        to_remove = Game.get(Game.name == arg.lower(), Game.played == False)
-        to_remove.played = True
-        to_remove.save()
-        return await ctx.send(f'Removed {arg}')
-    except Game.DoesNotExist:
-        to_remove = list(Game.select().where(Game.name % arg, Game.played == False))
-        if len(to_remove) > 1 or len(to_remove) < 1:
-            return await ctx.send(f'More than one game returned for "{arg}"')
-        else:
-            to_remove[0].played = True
-            to_remove[0].save()
-            return await ctx.send(f'Removed {arg}')
+async def finish(ctx, arg):
+    """Marks a game as finished"""""
+    game = _get_game(arg, ctx)
+    if game[0]:
+        game[1].started = True
+        game[1].finished = True
+        game[1].finished_on = datetime.now()
+        game[1].save()
+        return await ctx.send(f'Marked {arg} as finished')
+    else:
+        return await ctx.send(game[1])
+
+
+@bot.command()
+async def start(ctx, arg):
+    """Marks a game as started"""
+    game = _get_game(arg, ctx)
+    if game[0]:
+        game[1].started = True
+        game[1].started_on = datetime.now()
+        game[1].save()
+        return await ctx.send(f'Started {arg}')
+    else:
+        return await ctx.send(game[1])
 
 
 @bot.command()
 async def games(ctx):
-    """A short & tidy list of games"""
+    """A list of games to play"""
 
     def make_games_content(games_list):
         content = ''
         for g in games_list:
-            date_diff = (datetime.now() - g.added_on).days
-            date_diff_text = f' (added {(datetime.now() - g.added_on).days} ago)' if date_diff else ''
             if g.url:
-                content += f'[{g.name.title()}]({g.url}){date_diff_text}\n'
+                content += f'[{g.name.title()}]({g.url}){g.recent_activity}\n'
             else:
-                content += f'{g.name.title()}{date_diff_text}\n'
+                content += f'{g.name.title()}{g.recent_activity}\n'
         return content
 
     embed = discord.Embed(
         title='Current Games List',
         colour=discord.Colour(0xE5E242),
-        description='`?add castle crashers`\n`?remove castle crashers`\n`?games`\n`?game_links`'
+        # description='`?add castle crashers`\n`?remove castle crashers`\n`?start castle crashers`\n`?games`\n`?game_links`'
     )
-    recent_games = Game.select().where(Game.added_on >= (datetime.now() - timedelta(days=30)), Game.played == False)
-    recent_games_value = make_games_content(recent_games)
-    older_games = Game.select().where(Game.added_on < (datetime.now() - timedelta(days=30)), Game.played == False)
-    older_games_value = make_games_content(older_games)
+    new_games = Game.select().where(
+        # Game.added_on >= (datetime.now() - timedelta(days=30)),
+        Game.started == False,
+        Game.finished == False
+    ).order_by(-Game.added_on, Game.name)
+    started_games = Game.select().where(
+        # Game.added_on >= (datetime.now() - timedelta(days=30)),
+        Game.started == True,
+        Game.finished == False
+    ).order_by(-Game.started_on, Game.name)
+    new_games_value = make_games_content(new_games)
+    started_games_value = make_games_content(started_games)
 
-    embed.add_field(name='Recent Additions', value=recent_games_value, inline=False) if recent_games_value else None
-    embed.add_field(name='Old Games', value=older_games_value, inline=False) if older_games_value else None
+    embed.add_field(name='New Games', value=new_games_value, inline=False) if new_games_value else None
+    embed.add_field(name='Games in Progress', value=started_games_value, inline=False) if started_games_value else None
+
+    return await ctx.send(embed=embed)
+
+
+@bot.command()
+async def finished(ctx):
+    """A list of finished games"""
+
+    def make_games_content(games_list):
+        content = ''
+        for g in games_list:
+            if g.url:
+                content += f'[{g.name.title()}]({g.url}){g.recent_activity}\n'
+            else:
+                content += f'{g.name.title()}{g.recent_activity}\n'
+        return content
+
+    embed = discord.Embed(
+        title='Finished Games',
+        colour=discord.Colour(0xE5E242),
+    )
+    finished_games = Game.select().where(
+        Game.started == True,
+        Game.finished == True
+    ).order_by(-Game.finished_on, Game.name)
+    finished_games_value = make_games_content(finished_games)
+
+    embed.add_field(name='Finished Games', value=finished_games_value, inline=False) if finished_games_value else None
 
     return await ctx.send(embed=embed)
 
 
 @bot.command()
 async def game_links(ctx):
-    """A big and messy list of games"""
+    """Spams the URLs so you can see pictures"""
     for g in Game.select():
         if g.url:
             await ctx.send(f'{g.name.title()} ({g.url})')
         else:
             await ctx.send(f'{g.name.title()}')
-
-
-@bot.event
-async def on_message(message):
-    if message.author != bot.user:
-        if random.randint(1, 100) == 69:
-            _ = ''
-            a = TextCount.get(text='p00p')
-            for i in range(a.counter):
-                _ += 'p00p '
-            await message.channel.send(_)
-            a.counter += 1
-            a.save()
-    await bot.process_commands(message)
 
 
 bot.run(conf['API_SECRET'])
--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-import peewee
 import datetime
 
-DATABASE = 'test.db'
+import peewee
+
+DATABASE = 'test4.db'
 
 db = peewee.SqliteDatabase(DATABASE)
 
@@ -16,13 +17,42 @@ class TextCount(BaseModel):
     counter = peewee.IntegerField()
 
 
+# New Games
 class Game(BaseModel):
     name = peewee.CharField()
     added_by = peewee.CharField()
     added_on = peewee.DateTimeField(default=datetime.datetime.now)
-    played = peewee.BooleanField(default=False)
+    started = peewee.BooleanField(default=False)
+    started_on = peewee.DateTimeField(null=True)
+    finished = peewee.BooleanField(default=False)
+    finished_on = peewee.DateTimeField(null=True)
     url = peewee.CharField(null=True)
     steam_id = peewee.BigIntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
+
+    @property
+    def recent_activity(self):
+        def plural(in_num):
+            return '' if in_num == 1 else 's'
+
+        date = self.added_on if not self.started else self.started_on
+        if not self.started:
+            flag = 'added'
+        elif not self.finished:
+            flag = 'started'
+        else:
+            flag = 'finished'
+
+        prop = flag + '_on'
+        date_diff = (datetime.datetime.now() - getattr(self, prop)).days
+        date_diff_text = f' ({flag} {(datetime.datetime.now() - date).days} day' \
+                         f'{plural((datetime.datetime.now() - date).days)} ago)' if date_diff else ''
+        return date_diff_text
 
 
 db.connect()
